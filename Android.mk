21bf81bb8f1d7ee2b8fcd2ab8879b94ae7ebcff1
Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>
2026-07-07 14:04:34 +0800

    Revert "aston: Bring back firmware"
    This reverts commit fef3bd02a71058e613f07f76448e8f29010a53f1.
    Ensure compatibility with Ace 3.
    
    Signed-off-by: Weaponmasterjax <42597871+weaponmasterjax@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -3,32 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),aston)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,ffc94942d657671cad849c1f88d81738683d6315)
-$(call add-radio-file-sha1-checked,radio/aop.img,b1e162e9cec53bcadafc2483952c96dc7ae5fde1)
-$(call add-radio-file-sha1-checked,radio/aop_config.img,c797ef81e8dab8707a3557c94580c5f7c5dd0f89)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,e6dad113d38fbb4e18600224fff8d5ef2b99937a)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,7e235ef7f63b115cc8d28cd6607bdae39cb7c3f3)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,913f6974340514291c58c25a9b080573ec7bd526)
-$(call add-radio-file-sha1-checked,radio/dsp.img,28885c51ced35d72cb8f10a1e250fe7a8c427bab)
-$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,0d9cbd46cc6aefddfd2dd2f85a6ef776333584ee)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,e7df2ef18f9c73c835da93c6a6a6bb0e1ff41688)
-$(call add-radio-file-sha1-checked,radio/hyp.img,56e5406288f16dcbb0279ec2efd8e0744bf2a3d5)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,d13fee19130202a0e1ff2d8f4d7d424236cddaf1)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,645b7382b8c036c4e978037fc0c486a1a0437e99)
-$(call add-radio-file-sha1-checked,radio/modem.img,3a7edeb2f5221abd588a2e7d91eaedf70deabce8)
-$(call add-radio-file-sha1-checked,radio/oplus_sec.img,1c584c7b98f32a7ccceebbad7564e3bc0944ccb0)
-$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,2938341a211ab7753b923ab029eec73c939724e3)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,01aa2393523c9ef640aad9147019a1025151ad68)
-$(call add-radio-file-sha1-checked,radio/shrm.img,17dafc43a960a73b21857fea4f0dca505bd42746)
-$(call add-radio-file-sha1-checked,radio/splash.img,0e82d9c1514da572cd55af48928475681fc15797)
-$(call add-radio-file-sha1-checked,radio/tz.img,185e383166c03a67fc4bbc3b64cf05963861bae2)
-$(call add-radio-file-sha1-checked,radio/uefi.img,4330fecbdac06aa731b58602b8d46444c29d5869)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,240d341094a41b87d5697e958c3a915494c3f1f3)
-$(call add-radio-file-sha1-checked,radio/xbl.img,7320dad9432af37649193f9ab516d519ea4cf02c)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,48b95426e1b6b6cd3401ff67401d9c31e440837f)
-$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,8634f96783a9e85029c0794e7fd4a5b22f9f768b)
-
-endif
